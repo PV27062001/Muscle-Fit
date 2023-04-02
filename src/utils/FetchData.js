8d7146ac95fb822334044f3c58b40d2a1426ffff
praveen <100416844+PV27062001@users.noreply.github.com>
2023-04-02 10:44:18 +0530

    second commit

--- src/utils/FetchData.js
+++ src/utils/FetchData.js
@@ -4,7 +4,7 @@ export const exerciseOptions = {
   method: "GET",
   url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": '433553f24bmsha50b805cd75d11cp148f04jsnd97ce6629408',
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
