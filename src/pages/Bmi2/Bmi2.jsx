b38b65632e680829527736dc993a0cc3be929035
praveen <100416844+PV27062001@users.noreply.github.com>
2023-04-08 13:58:54 +0530

    third

--- src/pages/Bmi2/Bmi2.jsx
+++ src/pages/Bmi2/Bmi2.jsx
@@ -1,266 +1,383 @@
-import {React,useState} from 'react'
-import './bmi.css'
-import {list1a,list1b,list1c,list1d,list1e,list1f,list1g} from './BmiData'
-import Nutrition from '../Nutrition';
-import "../NutritionCalculator.css"
+  import {React,useState,useEffect} from 'react'
+  import './bmi.css'    
+  import {list1a,list1b,list1c,list1d,list1e,list1f,list1g,list2a,list2b,list2c,list2d,list2e,list2f,list2g} from './BmiData'
+  import Nutrition from '../Nutrition';
+  import "../NutritionCalculator.css"
+  import { Pie } from "react-chartjs-2";
 
+  let calorie=0;
+  let res=0;
+  let bmiResult = localStorage.getItem('bmiResult');
+  let bmiResults = [];
 
-let calorie=0;
-let res=0;
+  function Bmi2() {
+      const [bmi,setbmi]=useState({
+          bweight: "",
+          bheight: "",
+          bage: "",
+          bgender: "",
+          bphysicalActivity: 0,
+      })   
+      const [flag,setflag] = useState(false)
+      const [bmiFlag,setbmiFlag] = useState(false)
+      const [chartData, setChartData] = useState({});
 
-function Bmi2() {
-    
-    const [bmi,setbmi]=useState({
-        bweight: "",
-        bheight: "",
-        bage: "",
-        bgender: "",
-        bphysicalActivity: 0,
-    })
-    
-    const [flag,setflag] = useState(false)
-    const [bmiFlag,setbmiFlag] = useState(false)
-    const [chartData, setChartData] = useState({});
+      function onchange(event){
+          console.log(bmi);   
+          setbmi({
+              ...bmi,
+              [event.target.name]: event.target.value,
+          })
+          
+          return;
+      }  
 
-    
-    
-    function onchange(event){
-        console.log(bmi);
-       
-        setbmi({
-            ...bmi,
-            [event.target.name]: event.target.value,
-        })
-        
-        return;
-    }
-    
-    function onsubmit(event){
-        event.preventDefault()
+      function onsubmit(event) {
+        event.preventDefault();
         let w = parseFloat(bmi.bweight);
         let h = parseFloat(bmi.bheight);
-        res = w/(h*h);
+        res = w / (h * h);
+        res = Math.round(res * 100) / 100;
+      
+        localStorage.setItem('bmiResult', res);
 
-        res = Math.round(res * 100) / 100
-        document.querySelector("#bmiDisplay").innerHTML=`YOUR BMI: ${res}`;
-        
+        document.querySelector("#bmiDisplay").innerHTML = res < 18.5 ? `Your BMI is ${res} ,underweight` : res < 25
+          ? `Your BMI is ${res} ,Normalweight` : res < 30 ? `Your BMI is ${res} ,Overweight` : `Your BMI is ${res} ,Obese`;
         let g = bmi.bgender;
 
         console.log(g);
-        if(g==="F")
-            calorie=((655.1 + 9.6*parseFloat(bmi.bweight) + 190*parseFloat(bmi.bheight))/(4.7*parseInt(bmi.bage)))*bmi.bphysicalActivity;
-        else
-            calorie=((66.5 + 13.8*parseFloat(bmi.bweight) + 500*parseFloat(bmi.bheight))/(6.8*parseInt(bmi.bage)))*bmi.bphysicalActivity;
+        if (g === "F") calorie = ((655.1 + 9.6 * parseFloat(bmi.bweight) + 190 * parseFloat(bmi.bheight))  / (4.7 * parseInt(bmi.bage))) * bmi.bphysicalActivity;
 
-            setbmi({
-                bweight: "",
-                bheight: "",
-                bage: "",
-                bgender: "",
-                bphysicalActivity: 0,
-            })
-          
-            const data = {
-              labels: ['BMI', 'Remaining'],
-              datasets: [
-                {
-                  data: [res, 25 - res],
-                  backgroundColor: ['#FF6384', '#36A2EB'],
-                  hoverBackgroundColor: ['#FF6384', '#36A2EB']
-                }
-              ]
-            };
-            setChartData(data);
-            
+        else calorie = ((66.5 + 13.8 * parseFloat(bmi.bweight) + 500 * parseFloat(bmi.bheight))  / (6.8 * parseInt(bmi.bage))) * bmi.bphysicalActivity;
+        console.log(calorie);
+        setbmi({
+          bweight: "",
+          bheight: "",
+          bage: "",
+          bgender: "",
+          bphysicalActivity: 0,
+        });
+        const data = {
+          labels: ['BMI', 'Remaining'],
+          datasets: [
+            {
+              data: [res, 25 - res],
+              backgroundColor: ['#FF6384', '#36A2EB'],
+              hoverBackgroundColor: ['#FF6384', '#36A2EB']
+            }
+          ]
+        };
+        setChartData(data);
         setflag(true);
+      }
+      
+      function BmiCalculator()
+      {
+          return <div>
+      <h1><center>  BMI CALCULATOR</center></h1>
+          <form className="bmi-form" onSubmit={onsubmit}>
+        <h2>Weight</h2>
+        <input type="number" step="0.01" min="0" placeholder="Weight(kg)" name="bweight" value={bmi.bweight} onChange={onchange} required autoComplete="on"/>
+        <h2>Height</h2>
+        <input type="number" min="0" step="0.01" placeholder="Height(m)" name="bheight" value={bmi.bheight} onChange={onchange} required autoComplete="on"/>
+        <h2>Age</h2>
+        <input type="number" min="0" placeholder="Age" name="bage" value={bmi.bage} onChange={onchange} required autoComplete="on"/>
+        <h2>Gender</h2>
+        <br></br>
+        <div class="bmi-gender">
+        <label><input type="radio" name="bgender" value="M" onChange={onchange} required autoComplete="off"/>Male</label>
+        <label><input type="radio" name="bgender" value="F" onChange={onchange} required autoComplete="off"/>Female</label>  
+        </div>
+        <br></br>
+        <h2>Physical Activity</h2>
+        <br></br>
+        <div class="bmi-radio">
+        <label><input type="radio" name="bphysicalActivity" value="1" onChange={onchange}  />Low</label>
+        <label><input type="radio" name="bphysicalActivity" value="2" onChange={onchange}  />Medium</label>
+        <label><input type="radio" name="bphysicalActivity" value="3" onChange={onchange}  />Heavy</label>
+        </div>
+        <div class="bmi-buttons">
+        <button type="submit">Submit</button>
+        <button onClick={() => window.history.back()}>Back</button>
+        </div>      
+          </form>
+      </div>
+  }
 
-        return;
-    }
-    
-    
-    function BmiCalculator()
+  ///    UNDER Weight
+
+    function calorie1()
     {
-        return <div>
-    <h1><center>  BMI CALCULATOR</center></h1>
-        <form className="bmi-form" onSubmit={onsubmit}>
-      <h2>Weight</h2>
-      <input type="number" step="0.01" min="0" placeholder="Weight(kg)" name="bweight" value={bmi.bweight} onChange={onchange} required autoComplete="on"/>
-      <h2>Height</h2>
-      <input type="number" min="0" step="0.01" placeholder="Height(m)" name="bheight" value={bmi.bheight} onChange={onchange} required autoComplete="on"/>
-      <h2>Age</h2>
-      <input type="number" min="0" placeholder="Age" name="bage" value={bmi.bage} onChange={onchange} required autoComplete="on"/>
-      <h2>Gender</h2>
-      <br></br>
-      <div class="bmi-gender">
-      <label><input type="radio" name="bgender" value="M" onChange={onchange} required autoComplete="off"/>Male</label>
-      <label><input type="radio" name="bgender" value="F" onChange={onchange} required autoComplete="off"/>Female</label>  
-      </div>
-      <br></br>
-      <h2>Physical Activity</h2>
-      <br></br>
-      <div class="bmi-radio">
-      <label><input type="radio" name="bphysicalActivity" value="1" onChange={onchange} required autoComplete="off"/>Low</label>
-      <label><input type="radio" name="bphysicalActivity" value="2" onChange={onchange} required autoComplete="off"/>Medium</label>
-      <label><input type="radio" name="bphysicalActivity" value="3" onChange={onchange} required autoComplete="off"/>Heavy</label>
-      </div>
-      <div class="bmi-buttons">
-      <button type="submit">Submit</button>
-      <button onClick={() => window.history.back()}>Back</button>
-      </div>      
-        </form>
-    </div>
-}
-
-function calorie1()
-{
-  return(
-  <div className="card-header">
-              <h1>in development</h1>
-          </div>
-  );
-}
-
-
-function Calorie2() {
-  return (
-    <div className="calorie-container">
-      <div className="calorie-card">
-        <div className="card-header">
-          <h1>Sunday</h1>
-        </div>
-        <div className="card-body">
-          <ul>
-            {list1a.map((item) => {
-              return <li>{item}</li>
-            })}
-          </ul>
-        </div>
-      </div>
-      <div className="calorie-card">
-        <div className="card-header">
-          <h1>Monday</h1>
-        </div>
-        <div className="card-body">
-          <ul>
-            {list1b.map((item) => {
-              return <li>{item}</li>;
-            })}
-          </ul>
-        </div>
-      </div>
-      <div className="calorie-card">
-        <div className="card-header">
-          <h1>Tuesday</h1>
-        </div>
-        <div className="card-body">
-          <ul>
-            {list1c.map((item) => {
-              return <li>{item}</li>;
-            })}
-          </ul>
-        </div>
-      </div>
-      <div className="calorie-card">
-        <div className="card-header">
-          <h1>Wednesday</h1>
-        </div>
-        <div className="card-body">
-          <ul>
-            {list1d.map((item) => {
-              return <li>{item}</li>;
-            })}
-          </ul>
-        </div>
-      </div>
-      <div className="calorie-card">
-        <div className="card-header">
-          <h1>Thursday</h1>
-        </div>
-        <div className="card-body">
-          <ul>
-            {list1e.map((item) => {
-              return <li>{item}</li>;
-            })}
-          </ul>
-        </div>
-      </div>
-      <div className="calorie-card">
-        <div className="card-header">
-          <h1>Friday</h1>
-        </div>
-        <div className="card-body">
-          <ul>
-            {list1f.map((item) => {
-              return <li>{item}</li>;
-            })}
-          </ul>
-        </div>
-      </div>
-      <div className="calorie-card">
-        <div className="card-header">
-          <h1>Saturday</h1>
-        </div>
-        <div className="card-body">
-          <ul>
-            {list1g.map((item) => {
-              return <li>{item}</li>;
-            })}
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-
-  function calorie3()
-  {
-    return(
-    <div className="card-header">
-                <h1>in development</h1>
+      return (
+        <><div className="heading"><h1>2000 Calories/day </h1></div>
+        <div className="calorie-container">
+          <br></br>
+          <div className="calorie-card">
+            <div className="card-header">
+              <h1>Sunday</h1>
             </div>
+            <div className="card-body">
+              <ul>
+                {list2a.map((item) => {
+                  return <li>{item}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="calorie-card">
+            <div className="card-header">
+              <h1>Monday</h1>
+            </div>
+            <div className="card-body">
+              <ul>
+                {list2b.map((item) => {
+                  return <li>{item}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="calorie-card">
+            <div className="card-header">
+              <h1>Tuesday</h1>
+            </div>
+            <div className="card-body">
+              <ul>
+                {list2c.map((item) => {
+                  return <li>{item}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="calorie-card">
+            <div className="card-header">
+              <h1>Wednesday</h1>
+            </div>
+            <div className="card-body">
+              <ul>
+                {list2d.map((item) => {
+                  return <li>{item}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="calorie-card">
+            <div className="card-header">
+              <h1>Thursday</h1>
+            </div>
+            <div className="card-body">
+              <ul>
+                {list2e.map((item) => {
+                  return <li>{item}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="calorie-card">
+            <div className="card-header">
+              <h1>Friday</h1>
+            </div>
+            <div className="card-body">
+              <ul>
+                {list2f.map((item) => {
+                  return <li>{item}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="calorie-card">
+            <div className="card-header">
+              <h1>Saturday</h1>
+            </div>
+            <div className="card-body">
+              <ul>
+                {list2g.map((item) => {
+                  return <li>{item}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+        </div></>
+      );
+    }
+
+////     Normal Weigth
+
+    function calorie2()
+    {
+      return(
+        <div className="card-header">
+                  <h1>in development</h1>
+                  <br></br>
+              </div>
+      );
+    }
+
+ ////     over wEIGHT
+
+    function calorie3()
+    {
+      return(
+      <div className="card-header">
+                  <h1>in development</h1>
+                  <br></br>
+              </div>
+      );
+    }
+
+
+  ////    OBESE
+
+  function calorie4() {
+    return (
+      <><div className="heading"><h1>1600 Calories/day </h1></div><div className="calorie-container">
+        <div className="calorie-card">
+          <div className="card-header">
+            <h1>Sunday</h1>
+          </div>
+          <div className="card-body">
+            <ul>
+              {list1a.map((item) => {
+                return <li>{item}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+        <div className="calorie-card">
+          <div className="card-header">
+            <h1>Monday</h1>
+          </div>
+          <div className="card-body">
+            <ul>
+              {list1b.map((item) => {
+                return <li>{item}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+        <div className="calorie-card">
+          <div className="card-header">
+            <h1>Tuesday</h1>
+          </div>
+          <div className="card-body">
+            <ul>
+              {list1c.map((item) => {
+                return <li>{item}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+        <div className="calorie-card">
+          <div className="card-header">
+            <h1>Wednesday</h1>
+          </div>
+          <div className="card-body">
+            <ul>
+              {list1d.map((item) => {
+                return <li>{item}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+        <div className="calorie-card">
+          <div className="card-header">
+            <h1>Thursday</h1>
+          </div>
+          <div className="card-body">
+            <ul>
+              {list1e.map((item) => {
+                return <li>{item}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+        <div className="calorie-card">
+          <div className="card-header">
+            <h1>Friday</h1>
+          </div>
+          <div className="card-body">
+            <ul>
+              {list1f.map((item) => {
+                return <li>{item}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+        <div className="calorie-card">
+          <div className="card-header">
+            <h1>Saturday</h1>
+          </div>
+          <div className="card-body">
+            <ul>
+              {list1g.map((item) => {
+                return <li>{item}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+      </div></>
     );
   }
-  
 
-function Calorie(){
+    function Calorie() {
+      if (calorie < 18.5) {
+        return calorie1(); // Underweight
+      }
+      if (calorie >= 18.5 && calorie <= 24.9) {
+        return calorie2(); // Normal weight
+      }
+      if (calorie >= 25 && calorie <= 29.9) {
+        return calorie3(); // Overweight
+      }
+      if (calorie >= 30) {
+        return calorie4(); // Obese
+      }
+    }
 
-if(res < 25)
-return calorie1();
 
-if( res >= 25 && res <= 30)
-  return Calorie2();
+    // function CalorieToLoseWeight()
+    // {
+    //   if(calorie <18.5)
+    //   {
+    //     return calorie1();
+    //   }
+    // }
+    
+    // const [calorieFunction, setCalorieFunction] = useState(Calorie);
 
-if(res > 30)
-  return calorie3();
 
-}
-
-    return (
-        <div className="bmi-grid">
-            <div className="bmi-grid1">
-                <h1>BMI</h1>
-                <div className="nutrition-wrapper">
-                <Nutrition className="nutrition"></Nutrition>
-              </div>
-            <div className="bmi-calc" onClick={()=>{
-                setflag(false)
-                setbmiFlag(false)
-            }}></div>
-            </div>
-            <div className="bmi-grid2">
-            <div className="bmi-grid2-color">
-                { !bmiFlag && BmiCalculator() }
-                <div className="bmi-result">
-                {!bmiFlag && <div id="bmiDisplay"></div>}
-                <br></br>
-                {flag && !bmiFlag && <button onClick={()=>{setbmiFlag(true)} }>Click here for Diet Recommendation</button>}
-                </div>
-                {bmiFlag && Calorie()}
-            </div>
-            </div>
+  return (
+    <div className="bmi-grid">
+      <div className="bmi-grid1">
+        <h1>BMI</h1>
+        <div className="bmi-calc" onClick={() => {
+          setflag(false)
+          setbmiFlag(false)
+        }}></div>
+      </div>
+      <div className="bmi-grid2">
+        <div className="bmi-grid2-color">
+          {!bmiFlag && BmiCalculator()}
+          <div className="bmi-result">
+            {!bmiFlag && <div id="bmiDisplay"></div>}
+            {/* <div className='Calorie-result'>{"Your recommended daily Calorie intake is : " +  (Math.round(calorie)) + "k" }</div>  */}
+            <br></br>
+            {!bmiFlag ? (
+                <button onClick={() => setbmiFlag(true)}>Click here for Diet Recommendation</button>
+              ) : (
+                <>
+                  {Calorie()}
+                  <br/><br/>
+                  <button onClick={() => setbmiFlag(false)}>Go back</button>
+                </>
+              )}
+            {/* {chartData && <Pie data={chartData} />} */} 
+          </div>
+          <div className='bmi-result'>{"Your older BMI : " +  bmiResult}</div> 
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+  
+  } 
 
-export default Bmi2
+  export default Bmi2
--- src/pages/Bmi2/BmiData.js
+++ src/pages/Bmi2/BmiData.js
@@ -55,6 +55,61 @@ const list1g = [
     "Dinner (8:00-8:30PM)\t1 cup ghia vegetable + 1 chaptti + salad"
 ]
 
+const list2a = [
+    "Breakfast (305 calories): 1 serving Oatmeal with Fresh or Frozen (No Sugar Added) Fruit, 2 tbsp. chopped nuts, 1 cup coffee or tea",
+    "A.M. Snack (183 calories): 1 medium banana, 1 tbsp. peanut butter",
+    "Lunch (410 calories): Turkey-Avocado Sandwich on whole-grain bread, 1 medium apple",
+    "P.M. Snack (119 calories): 8 baby carrots, 2 tbsp. hummus",
+    "Dinner (518 calories): 1 serving Salmon with Roasted Vegetables, 1 medium whole-wheat dinner roll, 1 tbsp. butter",
+]
+
+const list2b = [
+    "Breakfast (266 calories): Avocado-Egg Toast, 1 large banana",
+    "A.M. Snack (85 calories): 1 medium orange",
+    "Lunch (357 calories): 2 cups Ravioli & Vegetable Soup, 1 small apple",
+    "P.M. Snack (169 calories): 6 dried apricots, 12 almonds",
+    "Dinner (566 calories): 1 serving Green Salad with Chicken & Strawberries, 2 tbsp. Honey-Mustard Vinaigrette, 1 medium whole-wheat dinner roll, 1 tbsp. butter",
+]
+
+const list2c = [
+    "Breakfast (274 calories): Peanut Butter-Banana English Muffin, 1 medium orange",
+    "A.M. Snack (62 calories): 2 medium celery stalks, 2 tbsp. hummus",
+    "Lunch (413 calories): 1 serving Veggie & Hummus Sandwich, 1 small apple",
+    "P.M. Snack (120 calories): 1 medium pear, 10 almonds",
+    "Dinner (595 calories): 1 serving Pork Chops with Roasted Vegetables, 1 cup cooked brown rice, 1/2 tbsp. olive oil",
+]
+
+const list2d = [
+    "Breakfast (290 calories): Greek Yogurt with Nectarine and Granola, 1 cup coffee or tea",
+    "A.M. Snack (95 calories): 1 medium apple, 1 string cheese",
+    "Lunch (412 calories): 1 serving Grilled Cheese with Tomato Soup, 1 medium orange",
+    "P.M. Snack (148 calories): 3/4 cup blueberries, 1/4 cup plain Greek yogurt",
+    "Dinner (655 calories): 1 serving Spaghetti Squash with Chicken, Pesto, and Roasted Tomatoes, 2 cups mixed greens, 2 tbsp. Italian salad dressing",
+]
+
+const list2e =[
+    "Breakfast (414 calories): 2 Banana-Pecan Pancakes, 2 Tbsp. maple syrup, 1 medium orange",
+    "A.M. Snack (249 calories): 1 hard-boiled egg, 1.5 oz. Cheddar cheese",
+    "Lunch (408 calories): 1 serving Chickpea & Quinoa Buddha Bowl, 1 oz. Cheddar cheese",
+    "P.M. Snack (305 calories): 1 medium apple, 2 Tbsp. peanut butter",
+    "Dinner (624 calories): 1 serving Moroccan-Spiced Chicken Thighs with Sweet Potato Wedges, 2 cups mixed greens, 1/4 cup Carrot-Ginger Vinaigrette",
+]
+
+const list2f = [
+    "Breakfast (426 calories): 1 serving Scrambled Tofu, 1 slice whole-wheat bread, toasted, 1 medium orange",
+    "A.M. Snack (268 calories): 1 medium apple, 2 Tbsp. peanut butter",
+    "Lunch (413 calories): 1 serving Chickpea & Quinoa Buddha Bowl, 1 oz. Cheddar cheese",
+    "P.M. Snack (292 calories): 1 medium banana, 20 roasted unsalted almonds",
+    "Dinner (555 calories): 1 serving Mushroom & Spinach Lasagna, 2 cups mixed greens, 2 Tbsp. Carrot-Ginger Vinaigrette",
+]
+
+const list2g = [
+    "Breakfast (389 calories): 1 serving Avocado-Egg Toast, 1 medium orange",
+    "A.M. Snack (268 calories): 1 medium apple, 2 Tbsp. peanut butter",
+    "Lunch (400 calories): 1 serving Chickpea & Quinoa Buddha Bowl, 1 oz. Cheddar cheese",
+    "P.M. Snack (288 calories): 1 medium banana, 20 roasted unsalted almonds",
+    "Dinner (582 calories): 1 serving Flank Steak with Chimichurri, 1 cup cooked quinoa, 2 cups mixed greens, 2 Tbsp. Carrot-Ginger Vinaigrette",
+]
 
 
-export {list1a,list1b,list1c,list1d,list1e,list1f,list1g}
+export {list1a,list1b,list1c,list1d,list1e,list1f,list1g,list2a,list2b,list2c,list2d,list2e,list2f,list2g}
